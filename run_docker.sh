cc4387f544c84c7828cb54cbb84e4cfec6def363
Samuel Chao <uel780425@gmail.com>
2024-12-29 23:35:59 +0800

    fix (docker): replace tensorflow image to 2.15.0 version
    1. Rename original Dockerfile to  Dockerfile_oldtemp
    2. New Dockerfile with tensorflow/tensorflow:2.18.0-gpu-jupyter as base image
    3. build_docker.sh modified:  image version unet_well:1.0.3
    4. run_docker.sh modified: container version uent_well_03

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,10 @@
 docker run -dit \
-    --name unet_well_01 \
+    --name unet_well_03 \
     -p 6006:6006 \
     -p 8888:8888 \
     -v ./:/workspace/ \
     -w /workspace/ \
     --gpus all \
     --shm-size 8G \
-    unet_well:1.0.1 \
+    unet_well:1.0.3 \
     sleep infinity
